4a76f99903dac584b4689a8a1b409d8358bc1d0c
Erik Ebsen <eebsen@expansioncapitalgroup.com>
2019-07-02 11:30:17 -0500

    Separate TaskList component

--- src/App.vue
+++ src/App.vue
@@ -12,49 +12,33 @@
           placeholder="Something need doing?"
           v-model="newTask"
           @keyup.enter="addTask"
-        >
+        />
       </section>
-      <section id="tasks">
-        <ul>
-          <li v-for="task in tasks" :class="{completed: task.completed}" :key="task.id">
-            <input :id="task.id" name="done" type="checkbox" @click="completeTask(task)">
-            <label>{{ task.title }}</label>
-            <button class="delete" @click="deleteTask(task)">&times;</button>
-          </li>
-        </ul>
-      </section>
-      <!-- <footer>{{ this.tasks.filter(t => !t.completed).length }} tasks left</footer> -->
-      <footer>{{ this.remainingTasks }}</footer>
+      <task-list
+        :tasks="this.tasks"
+        @complete-task="completeTask($event)"
+        @delete-task="deleteTask($event)"
+      ></task-list>
     </main>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import NewTask from '@/components/NewTask.vue';
-import Tasks from '@/components/Tasks.vue';
-
-interface Task {
-  id: number;
-  title: string;
-  completed: boolean;
-}
+import { Component, Vue, Watch } from "vue-property-decorator";
+import Task from "@/Task";
+import NewTask from "@/components/NewTask.vue";
+import TaskList from "@/components/TaskList.vue";
 
 @Component({
   components: {
     NewTask,
-    Tasks,
+    TaskList,
   },
 })
 export default class App extends Vue {
   protected currentId = 1;
-  protected newTask = '';
+  protected newTask = "";
   protected tasks: Task[] = [];
-
-  protected get remainingTasks() {
-    const num = this.tasks.filter(t => !t.completed).length
-    return `${num} task${num === 1 ? '' : 's'} left`
-  }
 
   protected addTask() {
     this.tasks.push({
@@ -63,18 +47,22 @@ export default class App extends Vue {
       completed: false,
     });
     this.currentId++;
-    this.newTask = '';
+    this.newTask = "";
   }
 
   protected completeTask(task: Task) {
-    // task.completed = true;
-    task.completed = !task.completed
+    task.completed = !task.completed;
   }
 
   protected deleteTask(task: Task) {
     const index = this.tasks.findIndex(t => t.id === task.id);
     this.tasks.splice(index, 1);
   }
+
+  // @Watch('tasks')
+  // protected onTaskChange() {
+  //   console.log(this.tasks);
+  // }
 }
 </script>
 
@@ -85,31 +73,19 @@ export default class App extends Vue {
   body
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
   main
-    // border-bottom: 1px dotted grey
     border-top: 1px dotted grey
     padding: 1em 0
-    // padding-bottom: 1em
   header h1
     margin-bottom: 0
-  // main
-  //   border-bottom: 1px solid grey
-  //   border-top: 1px solid grey
   li
-    // display: flex
-    // justify-content: space-between
     list-style-type: none
-    // button
-    //   float: right
   ul
     padding-left: 0
   .completed
-    // input::checked
     label
       text-decoration: line-through
   .delete
-    color: red
+    color: #E7040F
+    font-weight: bold
     float: right
-    // display: none
-    // &:hover
-  //   display: inline
 </style>
---
+++ src/components/TaskList.vue
@@ -0,0 +1,28 @@
+<template>
+  <article id="tasks">
+    <ul>
+      <li v-for="task in tasks" :class="{completed: task.completed}" :key="task.id">
+        <input :id="task.id" name="done" type="checkbox" @click="$emit('complete-task', task)" />
+        <label>{{ task.title }}</label>
+        <button class="delete" @click="$emit('delete-task', task)">&times;</button>
+      </li>
+    </ul>
+    <footer>{{ this.remainingTasks }}</footer>
+  </article>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+import Task from "@/Task";
+
+@Component
+export default class TaskList extends Vue {
+  protected currentId = 1;
+  @Prop() protected tasks!: Task[];
+
+  protected get remainingTasks() {
+    const num = this.tasks.filter(t => !t.completed).length;
+    return `${num} task${num === 1 ? "" : "s"} left`;
+  }
+}
+</script>
